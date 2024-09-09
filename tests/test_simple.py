1b6c1bcfbd4d065cf17b3cb305449ef267fe4176
Guillaume Le Pape <gui.lepape25@gmail.com>
2024-09-09 11:28:07 +0200

    Improve ingredient parsing with regex

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -18,6 +18,10 @@ from cooking.models import Ingredient
             "120 g saumon fumé",
             Ingredient(preposition="de ", name="saumon fumé", quantity=120.0, unit="g"),
         ),
+        (
+            "120.989g de saumon fumé",
+            Ingredient(preposition="de ", name="saumon fumé", quantity=120.989, unit="g"),
+        ),
         ("0.5 gousse ail", Ingredient(preposition="d'", name="ail", quantity=0.5, unit="gousse")),
         (
             "3 brin estragon",
@@ -35,6 +39,16 @@ from cooking.models import Ingredient
             "5 filet anchois à l'huile",
             Ingredient(preposition="d'", name="anchois à l'huile", quantity=5.0, unit="filet"),
         ),
+        (
+            "5filet d'anchois à l'huile",
+            Ingredient(preposition="d'", name="anchois à l'huile", quantity=5.0, unit="filet"),
+        ),
+        (
+            "1,5g de saumon fumé",
+            Ingredient(preposition="de ", name="saumon fumé", quantity=1.5, unit="g"),
+        ),
+        ("1 oignon", Ingredient(preposition="", name="oignon", quantity=1.0, unit="")),
+        ("1oignon", Ingredient(preposition="", name="oignon", quantity=1.0, unit="")),
     ],
 )
 def test_ingredient_parsing(test_input: str, expected_ingredient: Ingredient) -> None:
