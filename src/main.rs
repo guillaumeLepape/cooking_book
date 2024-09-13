46816ff618ed50669cb2f4bc7083042e28b4de26
Guillaume Le Pape <gui.lepape25@gmail.com>
2024-09-13 23:01:53 +0200

    Exclude main function from coverage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use cooking_book::create_app;
 use cooking_book::db;
 use cooking_book::DATABASE_URL;
 
+#[cfg(not(tarpaulin_include))]
 #[must_use]
 #[rocket::launch]
 pub fn rocket() -> rocket::Rocket<rocket::Build> {
