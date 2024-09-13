500f8d7b1ccec2246516207e31838e44081e34aa
Guillaume Le Pape <gui.lepape25@gmail.com>
2024-09-13 23:01:53 +0200

    Exclude main function from coverage

--- src/bin/create_recipes/main.rs
+++ src/bin/create_recipes/main.rs
@@ -2,6 +2,7 @@ use cooking_book::db;
 use cooking_book::script;
 use cooking_book::DATABASE_URL;
 
+#[cfg(not(tarpaulin_include))]
 fn main() {
     script::create_recipes(&mut db::DBConnection(
         db::connect(DATABASE_URL).get().unwrap(),
